406d843ae3640960af9ee885af1a7686a61a8aef
huseinreds <35657493+huseinreds@users.noreply.github.com>
2024-02-20 21:50:57 +0700

    Update main.go

--- main.go
+++ main.go
@@ -165,12 +165,12 @@ func invokeTx(a *account.Account) {
 		rsp, err := a.AddInvokeTransaction(context.Background(), InvokeTx)
 		if err != nil {
 			fmt.Println(err)
-			time.Sleep(10 * time.Second)
+			time.Sleep(6 * time.Second)
 			continue
 		}
 
 		fmt.Println("tx: ", rsp.TransactionHash, " nonce: ", nonce)
-		time.Sleep(10 * time.Second)
+		time.Sleep(6 * time.Second)
 	}
 }
 
